a0a1c324b818e3d8889cc0f3d691234902b12b5b
Prithieraj M R <prithieraj.ravikumar@solitontech.com>
2026-04-01 15:09:58 +0530

    Ran black formatter on tsmcontext.py and added few additional checks in test_multi_site.py for validating the functionality

--- tests/test_multi_site.py
+++ tests/test_multi_site.py
@@ -1,11 +1,11 @@
 import pytest
-
+from nitsm.codemoduleapi import SemiconductorModuleContext
 
 @pytest.mark.pin_map("multi_site.pinmap")
 class TestMultiSite:
     pin_map_dut_pins = ["DUTPin1"]
-    pin_map_dut_pins = ["DUTPin1"]
     pin_map_system_pins = ["SystemPin1", "SystemPin2"]
+    sites = 4 # [0, 1, 2, 3]
 
     def test_get_semiconductor_module_with_sites(self, standalone_tsm_context):
         # Get context for sites 1 and 3
@@ -22,5 +22,8 @@ class TestMultiSite:
         # Validate that a fully initialized SemiconductorModuleContext wrapper is returned
         assert isinstance(filtered_tsm_context, SemiconductorModuleContext)
         # Exercise another wrapper method to catch initialization regressions
-        pin_names = list(filtered_tsm_context.get_pin_names())
+        pin_names = list(filtered_tsm_context.get_pin_names()) # Should contain DUTPins and SystemPins as a tuple
         assert isinstance(pin_names, list)
+        # Each item is a tuple, change it to list and compare
+        assert list(pin_names[0]) == self.pin_map_dut_pins
+        assert list(pin_names[1]) == self.pin_map_system_pins
